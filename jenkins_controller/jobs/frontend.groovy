e9692b5e858d6ca3e4e57705dc6c55a3fd0d1b7b
zax75ster <zax75ster@gmail.com>
2023-04-05 14:46:53 +0000

    fix names

--- jenkins_controller/jobs/frontend.groovy
+++ jenkins_controller/jobs/frontend.groovy
@@ -5,7 +5,7 @@ pipelineJob('frontend') {
                 git {
                     remote {
                         credentials("")
-                        url("https://github.com/zax75ster/Fronted")
+                        url("https://github.com/zax75ster/Frontend")
                     }
                     branches('main')
                     scriptPath('Jenkinsfile')
